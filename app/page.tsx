df43107b241ad70cb16f741cab242540d6b9b01d
Ibidapo Ayomide <ibidapoayomide754@gmail.com>
2024-11-26 13:09:43 +0100

    Build issue solved

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/forms/LoginForm";
 
 export default function Home() {
   return (
-    <div className="bg-white rounded-[10px] shadow-lg max-w-[518px] mx-auto h-autp pt-5 pb-10 px-20">
+    <div className="bg-white rounded-[10px] shadow-lg max-w-[518px] mx-auto h-autp pt-5 pb-10 px-5 md:px-20">
       <h1 className="text-textColor text-[28px] font-normal text-center">Welcome back</h1>
 
       <LoginForm />
