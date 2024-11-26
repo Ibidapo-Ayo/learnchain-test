c389a0dbe209f9fe446ec99542a9fbd6b892320c
Ibidapo Ayomide <ibidapoayomide754@gmail.com>
2024-11-26 13:29:10 +0100

    Bug fixed

--- components/WalletConnect.tsx
+++ components/WalletConnect.tsx
@@ -12,7 +12,7 @@ const WalletConnect = () => {
     const handleConnectWallet = async () => {
         try {
             setIsLoading(true)
-            if (window.ethereum) {
+            if (typeof window !== "undefined" && window.ethereum) {
                 const web3 = new Web3(window.ethereum);
 
                 await window.ethereum.request({ method: 'eth_requestAccounts' });
